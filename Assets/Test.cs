26f20fcc994449177b4a1709d1da204180610722
青柳宏史 <foncllibenamgalkca@gmail.com>
2016-09-05 21:54:21 +0900

    課題提出
    レッスン４

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+		void Start (){
+		//要素の個数が5の、int型の配列arrayを宣言
+		int[] array = new int[5];
+		//好きな値
+		array [0] = 5;
+		array [1] = 4;
+		array [2] = 3;
+		array [3] = 2;
+		array [4] = 1;
+		//配列の各要素の値を順番に表示
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (array [i]);
+		}
+		for (int j = 4; j >= 0; j--) {
+			Debug.Log (array [j]); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss {
+	private int hp = 100;    // 体力
+	private int power = 25; // 攻撃力
+	private int mp = 53; //MP
+	private int down = 5; //消費MP
+
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage + "のダメージを受けた");
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
+	}
+	//魔法攻撃用の関数
+	public void Magic( int a) { 
+		for ( int b = 0; b <= a ; b++){
+			if (mp >= 5) {
+				this.mp -= this.down;
+				Debug.Log ("魔法攻撃をした。残りMPは" + this.mp);
+
+			} else { 
+				Debug.Log ("MPが足りないため魔法が使えない。");
+			}
+		}
+	}
+}
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+		// 攻撃用の関数を呼び出す
+		lastboss.Attack();
+		// 防御用の関数を呼び出す
+		lastboss.Defence(3);
+		// 魔法用の関数を呼び出す
+		lastboss.Magic(10);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
